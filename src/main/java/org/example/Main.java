32ed1fc079f4d28024cee914b7eeeae572cdf3de
Lorenz Held <held.lorenz.08@gmail.com>
2024-08-27 21:58:32 +0200

    Berufsmesse

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+
+        System.out.println(printer.combinder("Hello"));
+    }
+}
---
+++ src/main/java/org/example/Printer.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Printer {
+
+    public Printer() {
+    }
+
+    public String combinder(String text){
+        String fulltext;
+        fulltext = text + " World";
+        return fulltext;
+
+    }
+
+}
+
+
